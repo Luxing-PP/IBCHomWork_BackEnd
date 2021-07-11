bc19cc0fda413eac88a62baceb96cdc360d7495a
Garzhi <56151286+Luxing-PP@users.noreply.github.com>
2021-07-12 00:41:42 +0800

    Add files via upload
    文档补充

---
+++ docs/CJZ/IBCDatabase.sql
@@ -0,0 +1,109 @@
+drop database if exists IBCDatabase;
+create database if not exists IBCDatabase default charset utf8;
+
+use IBCDatabase;
+
+# tip i don'know= =
+SET NAMES utf8;
+SET FOREIGN_KEY_CHECKS=0;
+
+
+-- ----------------------------
+-- Table structure for `user`
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `uid` int NOT NULL AUTO_INCREMENT,
+#  tip Collate 影响的是排序的规则
+#      Engine = InnoDB 则是选择了一个带事务控制（全部执行或全部不执行）引擎，
+  `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci default NULL,
+  `userType` tinyint default 0,
+  `isActive` boolean,
+  PRIMARY KEY (`uid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES ('1','Luxing','0','0');
+
+-- ----------------------------
+-- Table structure for `user_info`
+-- ----------------------------
+DROP TABLE IF EXISTS `user_info`;
+CREATE TABLE `user_info` (
+    `infoID` int NOT NULL AUTO_INCREMENT,
+    `uid` int NOT NULL,
+    `version` int NOT NULL,
+    `saveTimes` int default 0,
+    `loginDay` int default 0,
+    `letterCount` int DEFAULT 0,
+    PRIMARY KEY (`infoID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- ----------------------------
+-- Records of user_info
+-- ----------------------------
+INSERT INTO `user_info` VALUES ('1','1','1','3000','5','0');
+-- ----------------------------
+-- Table structure for `user_letter`
+-- ----------------------------
+DROP TABLE IF EXISTS `user_letter`;
+CREATE TABLE `user_letter`(
+    `ulid` int NOT NULL AUTO_INCREMENT,
+    `uid` int NOT NULL ,
+    `lid` int NOT NULL ,
+    PRIMARY KEY (`ulid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- ----------------------------
+-- Records of user_letter
+-- ----------------------------
+# INSERT INTO `user_letter` VALUES ('1','1','1');
+
+-- ----------------------------
+-- Table structure for `letters`
+-- ----------------------------
+DROP TABLE IF EXISTS `letters`;
+CREATE TABLE `letters`(
+    `lid` int NOT NULL AUTO_INCREMENT,
+    `uid` int NOT NULL,
+    `content` varchar(2000) NOT NULL,
+    `created` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`lid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of letters
+-- ----------------------------
+# INSERT INTO `letters` (lid, uid, content) VALUES ('1','1','我是呆滞XXXXXX');
+-- ----------------------------
+-- Table structure for `response`
+-- ----------------------------
+DROP TABLE IF EXISTS `response`;
+CREATE TABLE `response`(
+    `rid` INT NOT NULL AUTO_INCREMENT,
+    `uid` INT NOT NULL ,
+    `lid` INT NOT NULL ,
+    `content` varchar(2000) NOT NULL,
+    PRIMARY KEY (`rid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- ----------------------------
+-- Records of response
+-- ----------------------------
+# Test GetResponse
+INSERT INTO `response` VALUES ('1','1','1','我是第一条评论');
+INSERT INTO `response` VALUES ('2','1','1','我是第二条评论');
+INSERT INTO `response` VALUES ('3','1','1','我是第三条评论');
+-- ----------------------------
+-- Table structure for `timers`
+-- ----------------------------
+DROP TABLE IF EXISTS `timers`;
+CREATE TABLE `timers`(
+    `tid` INT NOT NULL AUTO_INCREMENT,
+    `version` INT NOT NULL UNIQUE ,
+#     86400s == 1day
+    `livingTime` INT DEFAULT 86400,
+    PRIMARY KEY (tid)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+SET FOREIGN_KEY_CHECKS=1;
